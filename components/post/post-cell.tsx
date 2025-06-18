d4342b969ee4dbb2c37a23737c1ac7298cadfbf7
surya narayanan <66560926+isuryanarayanan@users.noreply.github.com>
2025-06-19 02:39:56 +0530

    deployment bugfix

--- components/post/post-cell.tsx
+++ components/post/post-cell.tsx
@@ -1,27 +1,18 @@
 import { Cell } from "@/types/post";
 import { MarkdownCell } from "./markdown-cell";
 import { ImageCell } from "./image-cell";
-import { ThumbnailCell } from "./thumbnail-cell";
 
 interface PostCellProps {
   cell: Cell;
-  className?: string;
 }
 
-export function PostCell({ cell, className }: PostCellProps) {
+export function PostCell({ cell }: PostCellProps) {
   switch (cell.type) {
     case "markdown":
       return <MarkdownCell content={cell.content as string} />;
     case "image":
       return (
         <ImageCell content={cell.content as { url: string; alt: string }} />
-      );
-    case "thumbnail":
-      return (
-        <ThumbnailCell
-          content={cell.content as { url: string; alt: string }}
-          className={className}
-        />
       );
     default:
       return null;
